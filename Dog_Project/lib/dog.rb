a1843793008ce74a04d08ea09a1bd269a93b9375
Michias Shiferaw <68077955+MichiasShiferaw@users.noreply.github.com>
2021-05-09 12:45:53 -0400

    Update dog.rb

--- Dog_Project/lib/dog.rb
+++ Dog_Project/lib/dog.rb
@@ -2,3 +2,7 @@ class Dog
     def initialize(name, breed, age, bark, favorite_foods)
         @name=name
         @breed=breed
+        @age=age
+        @bark=bark
+        @favorite_foods=favorite_foods
+    end
