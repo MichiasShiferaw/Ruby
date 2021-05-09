f41dc9fa202468b27639f1ccae815054fb123d5b
Michias Shiferaw <68077955+MichiasShiferaw@users.noreply.github.com>
2021-05-09 12:48:38 -0400

    Update dog_spec.rb

--- Dog_Project/spec/dog_spec.rb
+++ Dog_Project/spec/dog_spec.rb
@@ -7,3 +7,18 @@ describe "Dog" do
     it "should accept a name (string), breed (string), age (number), bark (string), and favorite_foods (array) as arguments" do
       dog
     end
+
+    it "should set the instance variables @name, @breed, @age, @bark, @favorite_foods" do
+      expect(dog.instance_variable_get(:@name)).to eq("Fido")
+      expect(dog.instance_variable_get(:@breed)).to eq("German Shepard")
+      expect(dog.instance_variable_get(:@age)).to eq(3)
+      expect(dog.instance_variable_get(:@bark)).to eq("Bork!")
+      expect(dog.instance_variable_get(:@favorite_foods)).to eq(["Bacon", "Chicken"])
+    end
+  end
+
+  describe "#name" do
+    it "should get (return) the dog's name instance variable" do
+      expect(dog.name).to be(dog.instance_variable_get(:@name))
+    end
+  end    
