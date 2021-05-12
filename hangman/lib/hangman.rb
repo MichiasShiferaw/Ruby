6188cd7ce833ac9f7da6437c0b9ad82918135478
Michias Shiferaw <68077955+MichiasShiferaw@users.noreply.github.com>
2021-05-12 13:48:39 -0400

    Update hangman.rb

--- hangman/lib/hangman.rb
+++ hangman/lib/hangman.rb
@@ -2,3 +2,10 @@ require "byebug"
 
 class Hangman
   DICTIONARY = ["cat", "dog", "bootcamp", "pizza"]
+
+  def initialize
+    @secret_word= Hangman.random_word
+    @guess_word=Array.new(secret_word.length, "_")
+    @attempted_chars=[]
+    @remaining_incorrect_guesses=5
+  end
