91f87c5d4ae0015ec5698a1bda3d000a350dba37
Michias Shiferaw <68077955+MichiasShiferaw@users.noreply.github.com>
2021-05-12 13:46:42 -0400

    Update guessing_game.rb

--- guessing_game/lib/guessing_game.rb
+++ guessing_game/lib/guessing_game.rb
@@ -7,3 +7,16 @@ class GuessingGame
         @num_attempts=0
         @game_over=false
     end
+    def check_num(num_1)
+
+        @num_attempts=@num_attempts+1
+
+         if num_1==@secret_num 
+            @game_over=true
+            return puts "you win" 
+         elsif num_1>@secret_num
+            p "too big"
+        else
+            p "too small"
+        end
+    end
