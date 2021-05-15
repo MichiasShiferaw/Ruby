90c6e09faef0d0f18218f029b284e18a89dce985
Michias Shiferaw <68077955+MichiasShiferaw@users.noreply.github.com>
2021-05-14 21:47:29 -0400

    Update code.rb

--- Mastermind_project/lib/code.rb
+++ Mastermind_project/lib/code.rb
@@ -11,3 +11,10 @@ class Code
   def self.valid_pegs?(arr1)
     return arr1.all? {|letter| POSSIBLE_PEGS.has_key?(letter.upcase)}
   end
+  def initialize(arr1)
+    if Code.valid_pegs?(arr1)
+      @pegs=arr1.map{|char| char.upcase}
+    else
+      raise "pegs are invalid"
+    end
+  end
