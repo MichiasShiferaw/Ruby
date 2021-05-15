f8e0b74718ce3ebca45c297c7839a65dfeb09aa4
Michias Shiferaw <68077955+MichiasShiferaw@users.noreply.github.com>
2021-05-14 21:46:33 -0400

    Update mastermind.rb

--- Mastermind_project/lib/mastermind.rb
+++ Mastermind_project/lib/mastermind.rb
@@ -9,3 +9,15 @@ class Mastermind
         @secret_code=Code.random(length)
 
     end
+      
+    def print_matches(instance)
+        # (0...instance.length).each do |i|
+        # if instance[i]==@secret_code[i]
+        #     p instance[i]
+        # end
+        # end
+
+        puts "exact matches: #{@secret_code.num_exact_matches(instance)}"
+        puts "near matches: #{@secret_code.num_near_matches(instance)}"
+
+    end
