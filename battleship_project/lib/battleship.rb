121a025cb7ab0101a44b7ca9e66b18ca6ce86234
Michias Shiferaw <68077955+MichiasShiferaw@users.noreply.github.com>
2021-05-18 20:10:39 -0400

    Update battleship.rb

--- battleship_project/lib/battleship.rb
+++ battleship_project/lib/battleship.rb
@@ -3,3 +3,9 @@ require_relative "player"
 
 class Battleship
     #attr_reader :player, :board
+
+    def initialize(n)
+        @player=Player.new
+        @board=Board.new(n)
+        @remaining_missing=@board.size*0.5
+    end
